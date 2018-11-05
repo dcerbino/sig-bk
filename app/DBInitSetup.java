9bc049dd955416f489ffa5dc15a7d7caf220770a
Sebastian Iglesias <sebastian.iglesias@mulesoft.com>
2018-11-04 21:13:08 -0300

    Added fine creation

--- app/DBInitSetup.java
+++ app/DBInitSetup.java
@@ -1,4 +1,5 @@
 import models.Company;
+import models.Fine;
 import models.Product;
 import models.ProductType;
 
@@ -23,6 +24,14 @@ public class DBInitSetup {
             product.name="P1";
             product.productType=ProductType.find.all().get(0);
             product.save();
+        }
+
+        if (Fine.find.all().isEmpty()) {
+            Fine fine = new Fine();
+            fine.amount = 4.05;
+            fine.currency = "pesos";
+            fine.reason = "Cause I say so";
+            fine.save();
         }
     }
 
