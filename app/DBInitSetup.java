a0cdc35755ad873ed73fa926163f1c1d476a2ef1
David Cerbino <dcerbino@dcerbino-ltm3.internal.salesforce.com>
2018-11-15 22:13:11 -0300

    removed dbinit lines

--- app/DBInitSetup.java
+++ app/DBInitSetup.java
@@ -5,27 +5,27 @@ import models.PurchaseOrder;
 
 public class DBInitSetup {
     public DBInitSetup() {
-        if (Delivery.find.all().isEmpty()){
-            Product product = new Product();
-            product.name="product1";
-            product.save();
-
-            ProviderCompany providerCompany = new ProviderCompany();
-            providerCompany.name="company1";
-            providerCompany.save();
-
-            Delivery delivery = new Delivery();
-            delivery.container="123454";
-            delivery.driverFullName="aaabbb";
-            delivery.save();
-
-            PurchaseOrder purchaseOrder = new PurchaseOrder();
-            purchaseOrder.product = product;
-            purchaseOrder.delivery = delivery;
-            purchaseOrder.provider= providerCompany;
-
-            purchaseOrder.save();
-        }
+//        if (Delivery.find.all().isEmpty()){
+//            Product product = new Product();
+//            product.name="product1";
+//            product.save();
+//
+//            ProviderCompany providerCompany = new ProviderCompany();
+//            providerCompany.name="company1";
+//            providerCompany.save();
+//
+//            Delivery delivery = new Delivery();
+//            delivery.container="123454";
+//            delivery.driverFullName="aaabbb";
+//            delivery.save();
+//
+//            PurchaseOrder purchaseOrder = new PurchaseOrder();
+//            purchaseOrder.product = product;
+//            purchaseOrder.delivery = delivery;
+//            purchaseOrder.provider= providerCompany;
+//
+//            purchaseOrder.save();
+//        }
     }
 
 }
